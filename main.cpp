dab5a20a707041f1ad0343204c7a84a04338352a
Soheil <soheilkhatibi1377@gmail.com>
2022-05-21 17:35:18 +0430

    Add main.cpp file

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char** argv){
+
+    // KMeans algorithm("Lenna.png", 6);
+    // EM algorithm("Lenna.png", 6);
+    // algorithm.cluster();
+
+    return 0;
+}
