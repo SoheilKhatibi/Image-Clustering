a17438d587f17e380c456ad1955a4b3bab54d29c
Soheil <soheilkhatibi1377@gmail.com>
2022-05-25 19:58:17 +0430

    Call cluster from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,20 @@
 int main(int argc, char** argv) {
 
     // KMeans algorithm("Lenna.png", 6);
-    // EM algorithm("Lenna.png", 6);
-    // algorithm.cluster();
+    EM algorithm("Lenna.png", 6);
+    algorithm.cluster();
 
     // arma::Cube<int> a(5, 4, 3, arma::fill::ones);
     // a.print("Cube:");
     // a(4, 3, 2) = 0;
     // a.slice(2).col(3).row(4).print("Matrix:");
 
-    cv::Mat img;
-    loadImage(img, "Lenna.png");
-    // displayImage(img);
-    arma::Mat<int> armaImage = opencv2arma(img);
-    cv::Mat opencvImage = arma2opencv(armaImage);
-    displayImage(opencvImage);
+    // cv::Mat img;
+    // loadImage(img, "Lenna.png");
+    // // displayImage(img);
+    // arma::Mat<int> armaImage = opencv2arma(img);
+    // cv::Mat opencvImage = arma2opencv(armaImage);
+    // displayImage(opencvImage);
 
     return 0;
 }
