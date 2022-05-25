b17deae98ea08173a024d62800347cfec39ff2a2
Soheil <soheilkhatibi1377@gmail.com>
2022-05-26 03:23:49 +0430

    Remove prints.

--- src/emImageClustering/emImageClustering.cpp
+++ src/emImageClustering/emImageClustering.cpp
@@ -28,15 +28,11 @@ cv::Mat EMImageClustering::reconstructImage() {
     cv::Mat reconstructedImage(img.rows, img.cols, CV_8UC3);
     for (int i = 0; i < reconstructedImage.rows; i++) {
         for(int j = 0; j < reconstructedImage.cols; j++) {
-            // std::cout << "img: " << img.at<cv::Vec3b>(i, j)[0] << " " << img.at<cv::Vec3b>(i,j)[1] << " " << img.at<cv::Vec3b>(i,j)[2] << std::endl;
             sample[0] = img.at<cv::Vec3b>(i, j)[0];
             sample[1] = img.at<cv::Vec3b>(i, j)[1];
             sample[2] = img.at<cv::Vec3b>(i, j)[2];
 
-            // std::cout << "sample: " << sample[0] << " " << sample[1] << " " << sample[2] << std::endl;
             nearestMeanIndex = minimumDistanceIndex(sample, means, nClusters);
-
-            // std::cout << "means: " << means[nearestMeanIndex][0] << " " << means[nearestMeanIndex][1] << " " << means[nearestMeanIndex][2] << std::endl;
 
             reconstructedImage.at<cv::Vec3b>(i, j)[0] = means[nearestMeanIndex][0];
             reconstructedImage.at<cv::Vec3b>(i, j)[1] = means[nearestMeanIndex][1];
