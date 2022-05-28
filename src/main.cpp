119ba0ef35843d7b26500a93bd80718bd6968a4b
Soheil <soheilkhatibi1377@gmail.com>
2022-05-28 05:53:57 +0430

    Get algorithm name as parameter from commandline input

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,23 @@ int main(int argc, char** argv) {
     //     std::cout << argv[i] << std::endl;
     // }
 
-    if (argc != 3) {
-        std::cout << "Usage: " << argv[0] << " <imageAddress> <nClusters>" << std::endl;
+    if (argc != 4) {
+        std::cout << "Usage: " << argv[0] << " <algorithm> <image address> <number of clusters>" << std::endl;
         return 1;
     }
 
-    // KMeansImageClustering algorithm(argv[1], atoi(argv[2]));
-    EMImageClustering algorithm(argv[1], atoi(argv[2]));
-    algorithm.cluster();
+    if (std::string(argv[1]) != "em" && std::string(argv[1]) != "kmeans") {
+        std::cout << "Algorithm should be either 'em' or 'kmeans'" << std::endl;
+        return 1;
+    }
+
+    if (std::string(argv[1]) == "em") {
+        EMImageClustering algorithm(argv[2], atoi(argv[3]));
+        algorithm.cluster();
+    } else if (std::string(argv[1]) == "kmeans") {
+        KMeansImageClustering algorithm(argv[2], atoi(argv[3]));
+        algorithm.cluster();
+    }
 
     // arma::Cube<int> a(5, 4, 3, arma::fill::ones);
     // a.print("Cube:");
